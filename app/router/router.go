0074540f40c121d135eb5cd6d0524db711356e8f
yosssi <yoshida.keiji.84@gmail.com>
2014-09-20 00:02:05 +0900

    Update app/router/router.go

--- app/router/router.go
+++ app/router/router.go
@@ -7,6 +7,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/yosssi/ace"
 	"github.com/yosssi/ace-proxy"
+	"github.com/yosssi/gcss"
 	"github.com/yosssi/orgs.io/app/controllers"
 	"github.com/yosssi/orgs.io/app/models"
 )
@@ -27,8 +28,22 @@ func New(config *models.Config) http.Handler {
 	router.GET("/", top.Index)
 
 	if config.App.Development() {
+		router.GET("/assets/*filepath", func(w http.ResponseWriter, r *http.Request, prms httprouter.Params) {
+			path := "app/assets" + prms[0].Value
+
+			if path == "app/assets/stylesheets/application.css" {
+				_, err := gcss.CompileFile(gcss.Path(path))
+
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+			}
+
+			http.ServeFile(w, r, path)
+		})
+
 		router.ServeFiles("/bower_components/*filepath", http.Dir("bower_components"))
-		router.ServeFiles("/assets/*filepath", http.Dir("app/assets"))
 	}
 
 	return router
